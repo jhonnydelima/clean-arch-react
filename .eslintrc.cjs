85bb87be18b1f79154ecb0540ac6d0ba6ad278c1
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-06-06 23:17:05 -0300

    chore: add vitest and setup tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,13 +8,15 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/consistent-type-definitions': "off",
-    '@typescript-eslint/strict-boolean-expressions': "off"
+    '@typescript-eslint/strict-boolean-expressions': "off",
+    'simple-import-sort/imports': "error",
+    'simple-import-sort/exports': "error"
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+    include: ["**/(*.)?{test,spec}.{ts,tsx}"],
+    exclude: ["node_modules", "dist", ".idea", ".git", ".cache"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "./test/coverage",
+      include: ["**/(*.)?{test,spec}.{ts,tsx}"],
+    },
+  },
+});
