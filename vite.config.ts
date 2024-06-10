02f7b2d97dc0fec4dfbc1d76779430791dc1fcd6
Jhonny Lima <jhonny.lima@funcionalcorp.com.br>
2024-06-09 21:08:09 -0300

    chore: setup path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 /// <reference types="vitest" />
+
+import path from "node:path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@data": path.resolve(__dirname, "./src/data"),
+      "@domain": path.resolve(__dirname, "./src/domain"),
+    },
+  },
   test: {
     globals: true,
     setupFiles: ["./test/setup.ts"],
